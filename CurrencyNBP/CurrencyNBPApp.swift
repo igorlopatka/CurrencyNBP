fb5388787f9174c10bf879459da21e7472f5674d
igorlopatka <igor.lopatka11@gmail.com>
2022-02-20 18:03:02 +0100

    Initial Commit

---
+++ CurrencyNBP/CurrencyNBPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyNBPApp.swift
+//  CurrencyNBP
+//
+//  Created by Igor Łopatka on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyNBPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
