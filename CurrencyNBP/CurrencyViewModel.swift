385cf7988bee612d702fdedf1d099d06f6b431c4
Igor Łopatka <igor.lopatka11@gmail.com>
2022-07-09 13:57:10 +0200

    Create universal networking method in ViewModel

--- CurrencyNBP/CurrencyViewModel.swift
+++ CurrencyNBP/CurrencyViewModel.swift
@@ -9,4 +9,33 @@ import Foundation
 
 class CurrencyViewModel: ObservableObject {
     
+    func getData(from urlString: String) async {
+        
+        var notTimelineRequest = false
+        if urlString.contains("tables") {
+            notTimelineRequest = true
+        }
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+
+            
+            if notTimelineRequest {
+                let standardResponse = try decoder.decode([Currency].self, from: data)
+//                    rates = standardResponse[0].rates
+                //            loadingState = .loaded
+            }
+            else {
+                let timelineResponse = try decoder.decode(CurrencyTimeline.self, from: data)
+            }
+        } catch {
+            //            loadingState = .failed
+        }
+    }
 }
